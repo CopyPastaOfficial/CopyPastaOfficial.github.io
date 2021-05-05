c136639d208970453406d0c775346708e75fce71
Remi ALBAN <remi@github.com>
2021-05-05 14:02:15 +0200

    Added spanish translations

---
+++ translations/es.js
@@ -0,0 +1,32 @@
+var es = {
+    home: "Inicio",
+    features: "Características",
+    how_to_use: "¿Cómo se utiliza?",
+    download: "Descargar",
+    about: "Una propuesta",
+    welcome: "Bienvenido a CopyPasta",
+    welcome_description: "CopyPasta es un proyecto que permite escanear un texto o una imagen (código QR, código de barras, reconocimiento de texto, ...) con el teléfono y enviarlo al ordenador.",
+    more: "Leer más",
+    
+    features_barcode: "Escanea códigos QR y códigos de barras",
+    features_text_recognition: "Reconocimiento de textos",
+    features_object_recognition: "Reconocimiento de objetos",
+    features_send: "Envía escaneos a su ordenador",
+
+    how_to_use_description: "Para utilizar esta aplicación, debes seguir estas sencillas instrucciones:Envía escaneos a su ordenador",
+    how_to_use_step_one: "Descargue la aplicación móvil y el software <a href=\"#download\">aquí</a>.",
+    how_to_use_step_two: "Abra el software y la aplicación móvil.",
+    how_to_use_step_three: "En su teléfono, haga clic en \"Agregar un dispositivo\" y escanee el código QR que aparece en la pantalla de su computadora.",
+    how_to_use_step_four: "Está terminado ! ¡Solo tiene que seleccionar su computadora en la aplicación móvil y ahora puede usar CopyPasta!",
+    how_to_use_tips: "Si no puede enviar sus escaneos a su computadora, asegúrese de que su teléfono esté conectado a la misma red WiFi que su computadora.",
+
+    download_windows_title: "Puede descargar el software aquí:",
+    download_windows_button: "Descarga para Windows",
+
+    download_android_title: "Puede descargar la aplicación de Android aquí:",
+
+    about_content: "Somos dos desarrolladores franceses.",
+
+    footer: "Este sitio está alojado en <a href=\"https://github.com\">Github</a>. El tema y los iconos utilizados en este sitio fueron creados por <a href=\"https://getbootstrap.com\">Bootstrap</a>."
+
+}
