3aa73ec30bb16c9cd547a242919d655abe855b63
Remi ALBAN <remi@github.com>
2021-05-05 16:23:18 +0200

    Added german traductions

---
+++ translations/de.js
@@ -0,0 +1,32 @@
+var de = {
+    home: "Begrüssung",
+    features: "Funktionalitäten",
+    how_to_use: "Wie funktioniert das?",
+    download: "Herunterladen",
+    about: "Zum Thema",
+    welcome: "Willkommen bei CopyPasta",
+    welcome_description: "CopyPasta ist ein Tool, mit dem Sie Text oder Bild (QR-Code, Barcode, Texterkennung, . . . ) mit Ihrem Handy scannen und an Ihren Computer senden können.",
+    more: "Weitere Informationen",
+    
+    features_barcode: "Scannt QR-Codes und Barcodes",
+    features_text_recognition: "Texterkennung",
+    features_object_recognition: "Erkennen von Gegenständen",
+    features_send: "Sendet die Scans an Ihren Computer",
+
+    how_to_use_description: "Um diese Anwendung zu verwenden, müssen Sie diese einfachen Anweisungen befolgen :",
+    how_to_use_step_one: "Laden Sie die mobile App und die Software herunter <a href=\"#download\">hier</a>.",
+    how_to_use_step_two: "Öffnen Sie die Software und die mobile App.",
+    how_to_use_step_three: "Klicken Sie auf Ihrem Handy auf \"Gerät hinzufügen\" und scannen Sie den QR-Code, der auf Ihrem Computerbildschirm angezeigt wird.",
+    how_to_use_step_four: "Es ist vorbei! Sie müssen nur Ihren Computer in der mobilen App auswählen und schon können Sie CopyPasta verwenden!",
+    how_to_use_tips: "Wenn Sie Ihre Scans nicht an Ihren Computer senden können, stellen Sie sicher, dass Ihr Telefon mit demselben WLAN-Netzwerk verbunden ist wie Ihr Computer.",
+
+    download_windows_title: "Sie können die Software hier herunterladen:",
+    download_windows_button: "Download für Windows",
+
+    download_android_title: "Sie können die Android App hier herunterladen:",
+
+    about_content: "Wir sind zwei französische Entwickler.",
+
+    footer: "Diese Seite wird von <a href=\"https://github.com\">Github</a> betrieben. Das Thema und die verwendeten Ikonen auf dieser Website wurden von <a href=\"https://getbootstrap.com\">Bootstrap</a> erstellt."
+
+}
